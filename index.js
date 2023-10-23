499332680d77415de4372f6c6a7c1d9f19459b5a
Sarah-Lisa Mero <sarahlisamero@gmail.com>
2023-10-23 12:04:03 +0200

    feat: get messages

--- index.js
+++ index.js
@@ -5,6 +5,23 @@ const cors = require("cors");
 app.use(cors());
 app.use(express.json()); // built-in express.json() middleware to parse JSON data
 
+app.get('/api/v1/messages', (req, res) => {
+    res.json({
+      status: "success",
+      message: "GET messages",
+      data: [
+        {
+          user: "Sarah",
+          message: "Hi",
+        },
+        {
+          user: "Lisa",
+          message: "Hello",
+        },
+      ],
+    });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
